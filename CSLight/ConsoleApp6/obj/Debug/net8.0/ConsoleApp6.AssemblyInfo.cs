72600d2b6ee1c8fe5e7ca6e62e8b66f02fd7ef8e
khrystoliubov <v.khristoliubov@red-beat.com>
2024-10-26 15:13:09 +0300

    programWithPasswordSolution

--- CSLight/ConsoleApp6/obj/Debug/net8.0/ConsoleApp6.AssemblyInfo.cs
+++ CSLight/ConsoleApp6/obj/Debug/net8.0/ConsoleApp6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab8a2a064c862aff7492b68863c44bb2a9aaacb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b4fd2c9b51e0c0b879576f8d09dd0b2da58a3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
