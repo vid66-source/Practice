b0b1d52edb32424bde89548b6835ebc64376c8fd
Khrystoliubov <v.khristoliubov@red-beat.com>
2024-10-29 23:38:57 +0200

    ArrayPractice

---
+++ CSLight/ArrayPractice1/ArrayPractice.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArrayPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] ints = { { 5, 4, 5, 2, 3, 73, 5 }, { 5, 16, 1, 3, 13, 1, 31 }, { 66, 6, 54, 48, 8, 4, 8 } };
+
+            for (int i = 0; i < ints.GetLength(0); i++)
+            {
+                for (int j = 0; j < ints.GetLength(1); j++)
+                {
+                    Console.WriteLine(ints[i, j]);
+                }
+            }
+        }
+    }
+}
--- CSLight/ArraysPractice/obj/Debug/net8.0/ArraysPractice.AssemblyInfo.cs
+++ CSLight/ArraysPractice/obj/Debug/net8.0/ArraysPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArraysPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88405999cef51999c485cff41fd8c021ecdf6ba1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67e1738264de12a0510e175d06d7b2136efd5bd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArraysPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArraysPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
