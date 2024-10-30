0a96fcbddf27b2d58db60d3cc671354291e04fba
khrystoliubov <v.khristoliubov@red-beat.com>
2024-10-30 21:53:01 +0200

    1stTaskSolution&2ndTaskCreation

--- CSLight/ArrayPractice1/ArrayPractice.cs
+++ CSLight/ArrayPractice1/ArrayPractice.cs
@@ -9,16 +9,28 @@ namespace ArrayPractice1
     {
         static void Main(string[] args)
         {
-            int[,] ints = { { 5, 4, 5, 2, 3, 73, 5 }, { 5, 16, 1, 3, 13, 1, 31 }, { 66, 6, 54, 48, 8, 4, 8 } };
+            int multipl = 1;
+            int sum = 0;
+            int[,] ints = new int[3, 7] { { 5, 4, 5, 2, 3, 73, 5 }, { 5, 16, 1, 3, 13, 1, 31 }, { 66, 6, 54, 48, 8, 4, 8 } };
 
             for (int i = 0; i < ints.GetLength(0); i++)
             {
                 for (int j = 0; j < ints.GetLength(1); j++)
                 {
                     Console.Write(ints[i, j] + " ");
+                    if (i == 1)
+                    {
+                        sum += ints[1, j];
+                    }
+                    if (j == 0)
+                    {
+                        multipl *= ints[i, 0];
+                    }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine(sum);
+            Console.WriteLine(multipl);
         }
     }
 }
