8583745d5faddcf4de412e471736f97c5c69666d
khrystoliubov <v.khristoliubov@red-beat.com>
2024-11-03 12:20:31 +0200

    ArrayPractice

--- CSLight/ArrayPractice2ndTask/Program.cs
+++ CSLight/ArrayPractice2ndTask/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace ArrayPractice2ndTask;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArraysPractice;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Random rand = new Random();
+
+        int maxInt = 0;
+
+        int[,] ints = new int[10, 10];
+
+        for (int i = 0; i < ints.GetLength(0); i++)
+        {
+            for (int j = 0; j < ints.GetLength(1); j++)
+            {
+                ints[i, j] = rand.Next(0, 100);
+                Console.Write(ints[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
--- CSLight/ArraysPractice/Program.cs
+++ CSLight/ArraysPractice/Program.cs
@@ -1,9 +1,55 @@
-﻿namespace ArraysPractice;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArraysPractice;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] tables = { 5, 9, 2, 8, 7, 6, 3, 1, 2 };
+        bool isOpen = true;
+
+        Console.Clear();
+        while (isOpen)
+        {
+            Console.SetCursorPosition(0, 23);
+            for (int i = 0; i < tables.Length; i++)
+            {
+                Console.WriteLine("Table " + (i + 1) + " has " + tables[i] + " free seats.");
+            }
+
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("Cafe administration\n\n1 - Order seat.\n\n2 - Exit\nSelect a menu item:");
+
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    int userTable, userPlace;
+                    Console.Write("Which table do you want to choose?:");
+                    userTable = Convert.ToInt32(Console.ReadLine()) - 1;
+                    if (tables.Length <= userTable || userTable < 0)
+                    {
+                        Console.WriteLine("Such a table does not exist.");
+                    }
+                    Console.Write("How many seats at this table do you need?:");
+                    userPlace = Convert.ToInt32(Console.ReadLine());
+                    tables[userTable] -= userPlace;
+                    if (tables[userTable] < userPlace)
+                    {
+                        Console.WriteLine("There are not enough seats at this table.");
+                        break;
+                    }
+                    break;
+                case 2:
+                    isOpen = false;
+                    break;
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
     }
 }
