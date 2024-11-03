2d13c1f812912c366c70e7d7c447c1025df8ac68
khrystoliubov <v.khristoliubov@red-beat.com>
2024-11-03 16:50:26 +0200

    3dTaskSolution

---
+++ CSLight/ArrayPractice3dTask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArraysPractice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int localMax = int.MinValue;
+        int[] ints = new int[30];
+        Random rand = new Random();
+
+        for (int i = 0; i < ints.Length; i++)
+        {
+            ints[i] = rand.Next(0, 25);
+            Console.Write(ints[i] + " ");
+        }
+        Console.WriteLine();
+        for (int i = 0; i < ints.Length; i++)
+        {
+            if (i >= 1)
+            {
+                if (ints[i] > ints[i - 1] && ints[i] > ints[i + 1])
+                {
+                    localMax = ints[i];
+                    Console.Write(localMax + " ");
+                }
+            }
+        }
+    }
+}
