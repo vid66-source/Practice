ca720665d17cbd7dd8951c76a945d52bab3084e7
khrystoliubov <v.khristoliubov@red-beat.com>
2024-11-10 22:27:42 +0200

    movement

--- CSLight/FunctionPractise/Program.cs
+++ CSLight/FunctionPractise/Program.cs
@@ -10,11 +10,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        char[,] map = Readmap("map1");
+        Console.CursorVisible = false;
 
-        int packmanX, packmanY;
+        bool isPlaying = true;
+
+        int pacmanX, pacmanY;
+        int pacmanDX = 0, pacmanDY = 1;
+
+        char[,] map = Readmap("map1", out pacmanX, out pacmanY);
 
         DrawMap(map);
+
+        while (isPlaying)
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                ChangeDirection(key, ref pacmanDX, ref pacmanDY);
+
+            }
+            if (map[pacmanX + pacmanDX, pacmanY + pacmanDY] != '#')
+            {
+                Move(ref pacmanX, ref pacmanY, pacmanDX, pacmanDY);
+            }
+            System.Threading.Thread.Sleep(150);
+        }
+
     }
 
     static void DrawMap(char[,] map)
@@ -24,13 +45,47 @@ class Program
             for (int j = 0; j < map.GetLength(1); j++)
             {
                 Console.Write(map[i, j]);
-            }   
+            }
             Console.WriteLine();
         }
     }
 
-    static char[,] Readmap(string manName)
+    static void Move(ref int X, ref int Y, int DX, int DY)
     {
+        Console.SetCursorPosition(Y, X);
+        Console.Write(' ');
+
+        X += DX;
+        Y += DY;
+
+        Console.SetCursorPosition(Y, X);
+        Console.Write('@');
+    }
+
+    static void ChangeDirection(ConsoleKeyInfo key, ref int DX, ref int DY)
+    {
+        switch (key.Key)
+        {
+            case ConsoleKey.UpArrow:
+                DX = -1; DY = 0;
+                break;
+            case ConsoleKey.DownArrow:
+                DX = 1; DY = 0;
+                break;
+            case ConsoleKey.LeftArrow:
+                DX = 0; DY = -1;
+                break;
+            case ConsoleKey.RightArrow:
+                DX = 0; DY = 1;
+                break;
+        }
+    }
+
+    static char[,] Readmap(string manName, out int pacmanX, out int pacmanY)
+    {
+        pacmanX = 0;
+        pacmanY = 0;
+
         string[] newFile = File.ReadAllLines($"Maps/{manName}.txt");
         char[,] map = new char[newFile.Length, newFile[0].Length];
 
@@ -38,9 +93,20 @@ class Program
         {
             for (int j = 0; j < map.GetLength(1); j++)
             {
-                map[i,j] = newFile[i][j];
+                map[i, j] = newFile[i][j];
+
+                if (map[i, j] == '@')
+                {
+                    pacmanX = i;
+                    pacmanY = j;
+                }
+                else
+                {
+                    
+                }
             }
         }
+
         return map;
     }
 }
