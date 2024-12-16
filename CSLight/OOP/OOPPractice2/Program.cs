84fcedf4b5da79d5a7aadc837e9cfb4a589569dd
khrystoliubov <v.khristoliubov@red-beat.com>
2024-12-16 21:00:24 +0200

    Has-A

---
+++ CSLight/OOP/OOPPractice2/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace OOPPractice2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        User user1 = new User("Jhon");
+        User user2 = new User("James");
+        List list = new List(new Task[] { new Task(user1, "Buy a milk."), new Task(user2, "Cook a dinner.") });
+
+        list.ShowAllTasks();
+        Console.ReadKey();
+    }
+
+    class User
+    {
+        public string Name;
+
+        public User(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class List
+    {
+        public Task[] Tasks;
+
+        public List(Task[] tasks)
+        {
+            Tasks = tasks;
+        }
+
+        public void ShowAllTasks()
+        {
+            for (int i = 0; i < Tasks.Length; i++)
+            {
+                Tasks[i].ShowInfo();
+            }
+        }
+    }
+
+    class Task
+    {
+        public User Worker;
+        public string Description;
+
+
+        public Task(User worker, string description)
+        {
+            Worker = worker;
+            Description = description;
+        }
+
+        public void ShowInfo()
+        {
+            System.Console.WriteLine($"Acting person {Worker.Name}\nTask description: {Description}");
+        }
+    }
+}
