a13f1850ce406f061392a65f1d98be601333ecb9
khrystoliubov <v.khristoliubov@red-beat.com>
2024-12-17 00:33:05 +0200

    getSet

---
+++ CSLight/OOP/OOPPractice4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace OOPPractice4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Renderer renderer = new Renderer();
+        Player player = new Player(5, 5);
+
+        renderer.DrawPlayer(player.X, player.Y);
+        Console.ReadKey();
+    }
+
+    class Renderer
+    {
+        public void DrawPlayer(int x, int y, char c = '@')
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+    }
+
+    class Player
+    {
+        public int X { get; private set; }
+
+        public int Y { get; private set; }
+
+        public Player(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+}
