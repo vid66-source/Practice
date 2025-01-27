f70c4c1c12b49be958bd3bd59334df633ea00289
Khrystoliubov <v.khristoliubov@red-beat.com>
2025-01-27 18:18:43 +0200

    store3

--- CSLight/OOP/OopHWFourthTask/OopHWFourthTask/Program.cs
+++ CSLight/OOP/OopHWFourthTask/OopHWFourthTask/Program.cs
@@ -1,42 +1,229 @@
-﻿namespace OopHWFourthTask;
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OopHWFourthTask;
+
+public static class WinUIManager
+{
+    public static int ScreenWidth, ScreenHeight;
+    static WinUIManager()
+    {
+        ScreenWidth = Console.LargestWindowWidth;
+        ScreenHeight = Console.LargestWindowHeight;
+    }
+
+    public static void WindowSizeOption(float screenShrinkRatioWidth, float screenShrinkRatioHight)
+    {
+
+        Console.SetWindowSize((int)(ScreenWidth * screenShrinkRatioWidth), (int)(ScreenHeight * screenShrinkRatioHight));
+        Console.SetBufferSize((int)(ScreenWidth * screenShrinkRatioWidth), (int)(ScreenHeight * screenShrinkRatioHight));
+    }
+
+    public static void UIElementHeadder(float xOffset, int yOffset)
+    {
+        Console.SetCursorPosition((int)(ScreenWidth * xOffset), yOffset);
+        Console.WriteLine("Welcome to our store. You can see a list of our goods at the right. Add goods into the cart and then buy them!");
+        Console.SetCursorPosition((int)(ScreenWidth * xOffset), yOffset + 1);
+        Console.WriteLine("______________________________________________________________________________________________________________");
+
+    }
+
+    public static void UIElementMenu(float xOffset, int yOffset)
+    {
+        Console.SetCursorPosition((int)(ScreenWidth * xOffset), yOffset);
+        Console.Write("Menu:");
+        Console.SetCursorPosition((int)(ScreenWidth * xOffset), yOffset + 1);
+        Console.Write("1)Add any product to your cart.");
+        Console.SetCursorPosition((int)(ScreenWidth * xOffset), yOffset + 2);
+        Console.Write("2)Purchase products in your cart.");
+        Console.SetCursorPosition((int)(ScreenWidth * xOffset), yOffset + 3);
+        Console.Write("Please choose your option");
+    }
+
+    public static void UIElementListHeadder(float xOffset, int yOffset, string text)
+    {
+
+        Console.SetCursorPosition((int)(ScreenWidth * xOffset), yOffset);
+        Console.Write(text);
+        Console.SetCursorPosition((int)(ScreenWidth * xOffset), yOffset + 1);
+        Console.Write("_________________________________________________________");
+    }
+
+    public static void SetCursor(float xOffset, int yOffset)
+    {
+        Console.SetCursorPosition((int)(ScreenWidth * xOffset), yOffset);
+    }
+    public static void SetCursor(float xOffset, int yOffset, string text)
+    {
+        Console.SetCursorPosition((int)(ScreenWidth * xOffset), yOffset);
+        Console.Write(text);
+    }
+}
+
+public static class ProductManager
+{
+    public static void RelocateProduct(List<Product> productsList1, List<Product> productslList2, string productName, float amount)
+    {
+        int productIndex = DetectProductIndex(productsList1, productName);
+        Product product = productsList1[productIndex];
+
+        if (amount <= product.ProductCount && product.IsPieceProduct)
+        {
+            product.UpdateCount(product.ProductCount - amount);
+            var productForCart = new Product(product.ProductName, amount, product.ProductPrice, true);
+            productslList2.Add(productForCart);
+        }
+
+        if (amount <= product.ProductCount && !product.IsPieceProduct)
+        {
+            if (amount % 1 != 0)
+            {
+                Console.WriteLine("Please enter valid amount.");
+            }
+            product.UpdateCount(product.ProductCount - amount);
+            var productForCart = new Product(product.ProductName, amount, product.ProductPrice, false);
+            productslList2.Add(productForCart);
+        }
+        else if (amount > product.ProductCount)
+        {
+            string weightOrPiece = product.IsPieceProduct ? "quantity" : "weight";
+            Console.WriteLine($"There is not enough product, please enter a different {weightOrPiece}.");
+        }
+    }
+
+
+    public static int DetectProductIndex(List<Product> products, string productName)
+    {
+        int productIndex = 0;
+        for (int i = 0; i < products.Count; i++)
+        {
+            if (products[i].ProductName == productName)
+            {
+                productIndex = i;
+                break;
+            }
+        }
+        return productIndex;
+    }
+
+    public static void SearchForDuplicate(List<Product> products)
+    {
+        for (int i = 0; i < products.Count; i++)
+        {
+            string productToCompare = products[i].ProductName;
+
+            for (int j = i + 1; j < products.Count; j++)
+            {
+                if (products[j].ProductName == productToCompare)
+                {
+                    products[i].UpdateCount(products[i].ProductCount + products[j].ProductCount);
+                    products.RemoveAt(j);
+                    j--;
+                }
+            }
+        }
+    }
+
+    public static void Availability(List<Product> products)
+    {
+        for (int i = products.Count - 1; i >= 0; i--)
+        {
+            if (products[i].ProductCount == 0)
+            {
+                products.RemoveAt(i);
+            }
+        }
+    }
+
+    public static void ShowProductsList<T>(float storePosX, int storePosY, List<Product> products, T owner) where T : class
+    {
+        foreach (var item in products)
+        {
+            WinUIManager.SetCursor(storePosX, storePosY, item.ProductShowInf(owner));
+            storePosY++;
+        }
+    }
+
+    public static void AddProductsToList(List<Product> productsList1, List<Product> productslList2)
+    {
+        productsList1.AddRange(productslList2);
+    }
+
+
+}
+
 
 class Program
 {
     static void Main(string[] args)
     {
+        bool storeIsOpen = true;
+        //store entities
+        Seller seller = new Seller(10000);
+        Customer customer = new Customer(1200);
+        Cart cart = new Cart();
+
+        //UI design
+
         if (OperatingSystem.IsWindows())
         {
-            Console.SetWindowSize(180, 50);
+            WinUIManager.WindowSizeOption(0.8f, 0.8f);
         }
         else
         {
-            Console.WriteLine("This feature is only supported on Windows.");
+            int screenWidth = 100;
+            int screenHeight = 100;
         }
-        bool storeIsOpen = true;
-        Seller seller = new Seller(1000);
-        Customer customer = new Customer(1200);
-        Cart cart = new Cart();
+
         while (storeIsOpen)
         {
-            Console.SetCursorPosition(25, 0);
-            Console.Write("Welcome to our store. You can see a list of our goods at the right. Add goods into the cart and then buy them!");
-            Console.SetCursorPosition(0, 1);
-            Console.Write("_______________________________________________________________________________________________________________________________________");
-            Console.SetCursorPosition(78, 3);
-            Console.Write("Store assortment:");
-            Console.SetCursorPosition(78, 4);
-            Console.Write("_________________________________________________________");
-            seller.SellerShowProducts(seller.SellerProducts, seller, 78, 5);
-            Console.SetCursorPosition(0, 2);
-            Console.Write("Menu:\n1)Add any product to your cart.\n2)Purchase products in your cart.");
-            customer.CustomerShowProducts(customer.CustomerProducts, customer, 0, 5);
+            //Headder
+            WinUIManager.UIElementHeadder(0.1f, 0);
+            //Menu
+            WinUIManager.UIElementMenu(0.1f, 2);
+            //Customer
+            WinUIManager.UIElementListHeadder(0.1f, seller.SellerProducts.Count + 5, "Your Purchases:");
+            ProductManager.ShowProductsList(0.4f, 4, customer.CustomerProducts, customer);
+            //Store
+            WinUIManager.UIElementListHeadder(0.5f, 2, "Store assortment:");
+            ProductManager.ShowProductsList(0.5f, 4, seller.SellerProducts, seller);
+            //Cart
+            WinUIManager.UIElementListHeadder(0.5f, seller.SellerProducts.Count + 5, "Your Cart:");
+            ProductManager.ShowProductsList(0.5f, seller.SellerProducts.Count + 7, cart.CartProducts, cart);
+            float cartProductsSum = cart.CartProducts.Count > 0 ? cart.TotalCost() : 0;
+            string cartProductsSumTxt = string.Format("{0:F2}", cartProductsSum);
+            WinUIManager.SetCursor(0.5f, seller.SellerProducts.Count + 7 + cart.CartProducts.Count);
+            Console.Write("Total cost: " + cartProductsSumTxt + "$");
+            WinUIManager.SetCursor(0.1f, 6);
 
             switch (Console.ReadLine())
             {
                 case "1":
-
+                    WinUIManager.SetCursor(0.1f, 7);
+                    Console.Write("Enter product name: ");
+                    string productName = Console.ReadLine();
+                    WinUIManager.SetCursor(0.1f, 8);
+                    Console.Write("Enter the quantity or weight of the product you wish to purchase: ");
+                    string amount = Console.ReadLine();
+                    ProductManager.RelocateProduct(seller.SellerProducts, cart.CartProducts, productName, float.Parse(amount));
+                    ProductManager.Availability(seller.SellerProducts);
+                    ProductManager.SearchForDuplicate(cart.CartProducts);
+                    Console.Clear();
                     break;
                 case "2":
+                    float customerMoney = customer.Money;
+                    float cartTotalCost = cart.TotalCost();
+                    if (cart.TotalCost() <= customer.Money)
+                    {
+                        customer.PayMoney(cart.TotalCost());
+                        ProductManager.AddProductsToList(cart.CartProducts, customer.CustomerProducts);
+                        ProductManager.SearchForDuplicate(customer.CustomerProducts);
+                        cart.CartProducts.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You don't have enough money.");
+                    }
 
                     break;
                 default:
@@ -54,6 +241,8 @@ class Seller
 
     public Seller(float money)
     {
+        Money = money;
+
         SellerProducts = new List<Product>{
             new Product("Balloons", 30f, 2.99f, true),
             new Product("Chocolate Bar", 15f, 8.50f, true),
@@ -69,29 +258,11 @@ class Seller
             new Product("Potato", 12.0f, 5.99f, false),
             new Product("Tomato", 1.0f, 5.99f, false)
         };
-        Money = money;
     }
 
-    public void SellerShowProducts(List<Product> products, Seller seller, int columnNum, int row)
+    public void GetMoney(float productsPrice)
     {
-        foreach (Product item in products)
-        {
-            Console.SetCursorPosition(columnNum, row);
-            item.ProductShowInf(seller);
-            Console.WriteLine();
-            row++;
-        }
-    }
-
-    public void Availability()
-    {
-        for (int i = SellerProducts.Count - 1; i >= 0; i--)
-        {
-            if (SellerProducts[i].ProductCount == 0)
-            {
-                SellerProducts.RemoveAt(i);
-            }
-        }
+        Money += productsPrice;
     }
 
 }
@@ -107,120 +278,39 @@ class Customer
         Money = money;
     }
 
-    public void BuyProductsFromCart(List<Product> productsFromCart)
+    public void PayMoney(float productsPrice)
     {
-        CustomerProducts.AddRange(productsFromCart);
-    }
-
-    public void SearchForDuplicate(List<Product> customerProducts)
-    {
-        for (int i = 0; i < customerProducts.Count; i++)
-        {
-            string productToCompare = customerProducts[i].ProductName;
-
-            for (int j = i + 1; j < customerProducts.Count; j++)
-            {
-                if (customerProducts[j].ProductName == productToCompare)
-                {
-                    customerProducts[i].UpdateCount(customerProducts[i].ProductCount + customerProducts[j].ProductCount);
-
-                    customerProducts.RemoveAt(j);
-
-                    j--;
-                }
-            }
-        }
-    }
-
-    public void CustomerShowProducts(List<Product> products, Customer customer, int columnNum, int row)
-    {
-        foreach (Product item in products)
-        {
-            Console.SetCursorPosition(columnNum, row);
-            item.ProductShowInf(customer);
-            Console.WriteLine();
-            row++;
-        }
+        Money -= productsPrice;
     }
 }
 
 class Cart
 {
-    public List<Product> ProductsInCart { get; private set; }
+    public List<Product> CartProducts { get; private set; }
 
     public Cart()
     {
-        ProductsInCart = new List<Product>();
-    }
-
-    public void AddProductToCart(List<Product> products, string productName, float amount)
-    {
-        int productIndex = DetectProductIndex(products, productName);
-        Product product = products[productIndex];
-
-        if (amount <= product.ProductCount && product.IsPieceProduct)
-        {
-            product.UpdateCount(product.ProductCount - amount);
-            var productForCart = new Product(product.ProductName, amount, product.ProductPrice, true);
-            ProductsInCart.Add(productForCart);
-        }
-
-        if (amount <= product.ProductCount && !product.IsPieceProduct)
-        {
-            if (amount % 1 != 0)
-            {
-                Console.WriteLine("Please enter valid amount.");
-            }
-            product.UpdateCount(product.ProductCount - amount);
-            var productForCart = new Product(product.ProductName, amount, product.ProductPrice, false);
-            ProductsInCart.Add(productForCart);
-        }
+        CartProducts = new List<Product>();
     }
 
     public float TotalCost()
     {
         float totalCost = 0;
-        foreach (var item in ProductsInCart)
+        foreach (var item in CartProducts)
         {
-            totalCost += item.ProductPrice;
+            totalCost += item.ProductPrice * item.ProductCount;
         }
         return totalCost;
     }
 
-    public void CartShowProducts(Customer customer)
-    {
-        foreach (var item in ProductsInCart)
-        {
-            item.ProductShowInf(customer);
-            Console.WriteLine();
-        }
-    }
-
-    private int DetectProductIndex(List<Product> products, string productName)
-    {
-        int productIndex = 0;
-        for (int i = 0; i < products.Count; i++)
-        {
-            if (products[i].ProductName == productName)
-            {
-                productIndex = i;
-                break;
-            }
-        }
-        return productIndex;
-    }
 }
 
-class Product
+public class Product
 {
     public string ProductName { get; private set; }
     public float ProductCount { get; private set; }
     public float ProductPrice { get; private set; }
     public bool IsPieceProduct { get; private set; }
-    public void UpdateCount(float newCount)
-    {
-        ProductCount = newCount;
-    }
 
     public Product(string productName, float weight, float productPrice, bool isPieceProduct)
     {
@@ -230,7 +320,12 @@ class Product
         IsPieceProduct = isPieceProduct;
     }
 
-    public void ProductShowInf(object person)
+    public void UpdateCount(float newCount)
+    {
+        ProductCount = newCount;
+    }
+
+    public string ProductShowInf(object owner)
     {
         string unit = IsPieceProduct ? "pcs" : "kg";
         string productDetails = $"Product: {ProductName}, ";
@@ -244,13 +339,12 @@ class Product
             productDetails += $"Weight: {ProductCount} {unit}";
         }
 
-        if (person is Seller)
+        if (owner is Seller)
         {
-            productDetails += $", Price: {ProductPrice}.";
+            productDetails += $", Price: {ProductPrice}$";
         }
 
-        Console.Write(productDetails);
+        return productDetails;
     }
-
 
 }
